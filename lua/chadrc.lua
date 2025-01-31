099f592d705c4ef3cc88f672d08a29f5fc58f55d
Dimitri Wijesinghe <dimitri.wijesinghe@gmail.com>
2025-01-30 20:03:02 -0500

    Customized NvChad

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,15 +1,84 @@
--- This file  needs to have same structure as nvconfig.lua
--- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
+-- This file needs to have same structure as nvconfig.lua
+-- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
-local highlights = require "highlights"
+M.base46 = {
+  theme = "chadracula", -- default theme
+  hl_add = {},
+  integrations = {},
+  changed_themes = {},
+  transparency = false,
+  theme_toggle = { "chadracula", "one_light" },
+
+  hl_override = {
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+  },
+}
+
+M.nvdash = {
+  load_on_startup = false,
+
+  header = {
+    "          -                  :         ",
+    "        -+*=                 ++:       ",
+    "      -+*++++:               ++*+:     ",
+    "    -+*+++++++-              +++++=:   ",
+    "  :-+++++++++++=             =++++++=: ",
+    " -=--=++++++++=++:           =++++++++=",
+    ":=----=++++======+-          =+++++++++",
+    " -------+++=======+=         =++++++++=",
+    " --------=+========+=:       =++++++++=",
+    " ----------===========-      =+========",
+    " ---------: ===========-     ==========",
+    " ---------:  -===========:   ==========",
+    " ---------:   :===========:  ==========",
+    " ---------:    :===========- -=========",
+    " ---------:      -=====================",
+    " ---------:       :====================",
+    " ---------:        :===================",
+    " -:-----:-:          -=================",
+    " :-::::::-:           :============-==-",
+    "  :::::::::            :============-: ",
+    "    :::::::              -=========:   ",
+    "      :::::               :=-====:     ",
+    "        :::                :-==:       ",
+    "                             :         ",
+    "                                       ",
+    "                                       ",
+    "                                       ",
+    "                                       ",
+  },
+}
+
+M.base46 = {
+  theme = "doomchad", -- default theme
+  hl_add = {},
+  integrations = {},
+  changed_themes = {},
+  transparency = false,
+  theme_toggle = { "chadracula", "one_light" },
+
+  hl_override = {
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+  },
+}
 
 M.ui = {
-  hl_override = highlights.override,
+  cmp = {
+    icons = true,
+    lspkind_text = true,
+    style = "default", -- default/flat_light/flat_dark/atom/atom_colored
+  },
+
+  telescope = { style = "borderless" }, -- borderless / bordered
+
   statusline = {
-    theme = "default", -- default/vscode/vscode_colored/minimal
+    theme = "minimal", -- default/vscode/vscode_colored/minimal
     -- default/round/block/arrow separators work only for default statusline theme
     -- round and block will work for minimal theme only
     separator_style = "block",
@@ -17,61 +86,12 @@ M.ui = {
     modules = nil,
   },
 
-  nvdash = {
-    load_on_startup = true,
-
-    header = {
-      "                                     -                  :                                 ",
-      "                                   -+*=                 ++:                               ",
-      "                                 -+*++++:               ++*+:                             ",
-      "                               -+*+++++++-              +++++=:                           ",
-      "                             :-+++++++++++=             =++++++=:                         ",
-      "                            -=--=++++++++=++:           =++++++++=                        ",
-      "                           :=----=++++======+-          =+++++++++                        ",
-      "                            -------+++=======+=         =++++++++=                        ",
-      "                            --------=+========+=:       =++++++++=                        ",
-      "                            ----------===========-      =+========                        ",
-      "                            ---------: ===========-     ==========                        ",
-      "                            ---------:  -===========:   ==========                        ",
-      "                            ---------:   :===========:  ==========                        ",
-      "                            ---------:    :===========- -=========                        ",
-      "                            ---------:      -=====================                        ",
-      "                            ---------:       :====================                        ",
-      "                            ---------:        :===================                        ",
-      "                            -:-----:-:          -=================                        ",
-      "                            :-::::::-:           :============-==-                        ",
-      "                             :::::::::            :============-:                         ",
-      "                               :::::::              -=========:                           ",
-      "                                 :::::               :=-====:                             ",
-      "                                   :::                :-==:                               ",
-      "                                                        :                                 ",
-      "                                                                                          ",
-      "                                                                                          ",
-      "                                                                                          ",
-      "                                                             ...                          ",
-      "                                                            .:::.                         ",
-      "                                                                                          ",
-      ".    .....         ......        .......    :::.       .::: .:.:   :::  :::::.  .::::::.  ",
-      "::..      :.    .:.      ..    ...     ..:  .:::.      :::. .:::.  :::::...:::::::...:::: ",
-      ".:         :.  .:         .:  :.         .:  .:::     :::.  .:::.  ::::     .:::.     :::.",
-      ".:         :.  :...........: .:           ::  ::::   :::.   .:::.  ::::     .:::      :::.",
-      ".:         :.  :             .:           ::   :::. .:::    .:::.  ::::     .:::      :::.",
-      ".:         :.  :.             :           :.    :::.:::     .:::.  ::::     .:::      :::.",
-      "::         :.   :.            .:         :.     .:::::      .:::.  ::::     .:::      :::.",
-      ".:         :.    ..........     .... ....        .:::.      .:::   ::::     .:::      :::.",
-      "                                                                                          ",
-      "                                                                                          ",
-    },
-
-    buttons = {
-      { "  Find File", "Spc f f", "Telescope find_files" },
-      { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
-      { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
-      { "  Bookmarks", "Spc m a", "Telescope marks" },
-      { "  Themes", "Spc t h", "Telescope themes" },
-      { "  Mappings", "Spc c h", "NvCheatsheet" },
-    },
+  -- lazyload it when there are 1+ buffers
+  tabufline = {
+    enabled = false,
   },
 }
+M.lsp = { signature = true }
+M.mason = { cmd = true, pkgs = {} }
 
 return M
---
+++ lua/plugins/init.lua
@@ -0,0 +1,133 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre", -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+      },
+    },
+  },
+
+  -- Preferred Dashboard
+  {
+    "goolord/alpha-nvim",
+    lazy = false,
+    dependencies = { "echasnovski/mini.icons" },
+    config = function()
+      local alpha = require "alpha"
+      local dashboard = require "alpha.themes.dashboard"
+
+      -- Custom ASCII Header
+      dashboard.section.header.val = {
+        "           -                  :          ",
+        "         -+*=                 ++:        ",
+        "       -+*++++:               ++*+:      ",
+        "     -+*+++++++-              +++++=:    ",
+        "   :-+++++++++++=             =++++++=:  ",
+        "  -=--=++++++++=++:           =++++++++= ",
+        " :=----=++++======+-          =+++++++++ ",
+        "  -------+++=======+=         =++++++++= ",
+        "  --------=+========+=:       =++++++++= ",
+        "  ----------===========-      =+======== ",
+        "  ---------: ===========-     ========== ",
+        "  ---------:  -===========:   ========== ",
+        "  ---------:   :===========:  ========== ",
+        "  ---------:    :===========- -========= ",
+        "  ---------:      -===================== ",
+        "  ---------:       :==================== ",
+        "  ---------:        :=================== ",
+        "  -:-----:-:          -================= ",
+        "  :-::::::-:           :============-==- ",
+        "   :::::::::            :============-:  ",
+        "     :::::::              -=========:    ",
+        "       :::::               :=-====:      ",
+        "         :::                :-==:        ",
+        "                              :          ",
+        "                                         ",
+        "                                         ",
+        "                                         ",
+      }
+      alpha.setup(dashboard.config)
+    end,
+  },
+
+  -- Preferred Tabline
+  {
+    {
+      "akinsho/bufferline.nvim",
+      event = "VeryLazy",
+      dependencies = "nvim-tree/nvim-web-devicons",
+      config = function()
+        local colors = require("base46").get_theme_tb "base_30"
+
+        require("bufferline").setup {
+          options = {
+            mode = "buffers", -- Show buffers instead of tabs
+            numbers = "none",
+            close_command = "bdelete! %d",
+            right_mouse_command = "bdelete! %d",
+            left_mouse_command = "buffer %d",
+            diagnostics = "nvim_lsp",
+            separator_style = "slant",
+            show_buffer_icons = true,
+            show_close_icon = false,
+            show_tab_indicators = true,
+            always_show_bufferline = true,
+          },
+          highlights = {
+            fill = {
+              fg = colors.light_grey, -- Background of the bufferline
+              bg = colors.black2, -- Background of the bufferline
+            },
+            background = {
+              fg = colors.light_grey, -- Inactive buffer text
+              bg = colors.black2, -- Inactive buffer background
+            },
+            buffer_selected = {
+              fg = colors.white, -- Active buffer text
+              bg = colors.black, -- Active buffer background
+            },
+            buffer_visible = {
+              fg = colors.light_grey, -- Inactive buffer text
+              bg = colors.black2, -- Inactive buffer background
+            },
+            separator = {
+              fg = colors.black2, -- Separator color
+              bg = colors.black2,
+            },
+            separator_selected = {
+              fg = colors.black, -- Separator when buffer is active
+              bg = colors.black,
+            },
+            close_button = {
+              fg = colors.light_grey,
+              bg = colors.black2,
+            },
+            close_button_selected = {
+              fg = colors.red, -- Close button on active tab
+              bg = colors.black,
+            },
+          },
+        }
+      end,
+    },
+  },
+}
