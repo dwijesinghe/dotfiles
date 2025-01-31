ea74620cfc50089146eeff87c9fe532ba22d85c1
Dimitri Wijesinghe <dimitri.wijesinghe@gmail.com>
2025-01-30 21:54:07 -0500

    Switch to dashboard-nvim

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,6 +8,10 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("n", "gt", ":BufferLineCycleNext<CR>", { noremap = true, silent = true })
 map("n", "<leader>bp", ":BufferLinePick<CR>", { noremap = true, silent = true })
+map("n", "<tab>", "gt", { noremap = true, silent = true })
+map({ "n", "t" }, "<C-j>", function()
+  require("nvchad.term").toggle { pos = "float", id = "floatTerm" }
+end, { desc = "terminal toggle floating term" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
