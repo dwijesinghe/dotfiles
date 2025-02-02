1204f6263fb17acc0e32378cbfddaab6c739384d
Dimitri Wijesinghe <dimitri.wijesinghe@gmail.com>
2025-02-02 15:13:10 -0500

    Update init.lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -15,6 +15,43 @@ return {
 
   {
     "nvim-treesitter/nvim-treesitter",
+    config = function()
+      local nvimtree = require "nvim-tree"
+      local colors = require("base46").get_theme_tb "base_30"
+
+      -- Apply Base46 colors to NvimTree highlights
+      vim.api.nvim_set_hl(0, "NvimTreeFolderIcon", { fg = colors.blue })
+      vim.api.nvim_set_hl(0, "NvimTreeFolderName", { fg = colors.blue })
+      vim.api.nvim_set_hl(0, "NvimTreeOpenedFolderName", { fg = colors.cyan, bold = true })
+      vim.api.nvim_set_hl(0, "NvimTreeEmptyFolderName", { fg = colors.light_grey })
+      vim.api.nvim_set_hl(0, "NvimTreeIndentMarker", { fg = colors.grey })
+      vim.api.nvim_set_hl(0, "NvimTreeExecFile", { fg = colors.green })
+      vim.api.nvim_set_hl(0, "NvimTreeGitDirty", { fg = colors.orange })
+      vim.api.nvim_set_hl(0, "NvimTreeGitNew", { fg = colors.yellow })
+      vim.api.nvim_set_hl(0, "NvimTreeGitDeleted", { fg = colors.red })
+      -- Setup NvimTree with recommended settings
+      nvimtree.setup {
+        renderer = {
+          highlight_git = true, -- Enable Git status highlighting
+          highlight_opened_files = "name", -- Highlight opened files
+          icons = {
+            show = {
+              git = true,
+              folder = true,
+              file = true,
+              folder_arrow = true,
+            },
+          },
+        },
+        view = {
+          width = 35,
+          side = "left",
+        },
+        git = {
+          enable = true, -- Enable Git integration
+        },
+      }
+    end,
     opts = {
       ensure_installed = {
         "vim",
@@ -32,37 +69,49 @@ return {
     event = "VimEnter",
     dependencies = { { "nvim-tree/nvim-web-devicons" } },
     config = function()
+      -- Apply Base46 colors to Dashboard highlights
+      local colors = require("base46").get_theme_tb "base_30"
+      vim.api.nvim_set_hl(0, "DashboardHeader", { fg = colors.blue })
+      vim.api.nvim_set_hl(0, "DashboardButton", { fg = colors.red, bold = true })
+      vim.api.nvim_set_hl(0, "DashboardShortcut", { fg = colors.red, bold = true })
+      vim.api.nvim_set_hl(0, "DashboardFooter", { fg = colors.blue })
       require("dashboard").setup {
+        hide = {
+          tabline = false,
+        },
         -- config
         config = {
           header = {
-            "           -                  :          ",
-            "         -+*=                 ++:        ",
-            "       -+*++++:               ++*+:      ",
-            "     -+*+++++++-              +++++=:    ",
-            "   :-+++++++++++=             =++++++=:  ",
-            "  -=--=++++++++=++:           =++++++++= ",
-            " :=----=++++======+-          =+++++++++ ",
-            "  -------+++=======+=         =++++++++= ",
-            "  --------=+========+=:       =++++++++= ",
-            "  ----------===========-      =+======== ",
-            "  ---------: ===========-     ========== ",
-            "  ---------:  -===========:   ========== ",
-            "  ---------:   :===========:  ========== ",
-            "  ---------:    :===========- -========= ",
-            "  ---------:      -===================== ",
-            "  ---------:       :==================== ",
-            "  ---------:        :=================== ",
-            "  -:-----:-:          -================= ",
-            "  :-::::::-:           :============-==- ",
-            "   :::::::::            :============-:  ",
-            "     :::::::              -=========:    ",
-            "       :::::               :=-====:      ",
-            "         :::                :-==:        ",
-            "                              :          ",
-            "                                         ",
-            "                                         ",
-            "                                         ",
+            "                                             ",
+            "                                             ",
+            "          💹🔫                ️🔫             ",
+            "         🈯️🈯️🈯️🌴              💹🍀          ",
+            "       💹💹💹🤢🍀              🐍🈯️🈯️🔫      ",
+            "  🎣🧩🈯️🈯️🈯️💹🈯️🔫             ⛳️💹🥎💹🔫    ",
+            "  🎽🥶💹🥗🈯️🐲💹💹🌴             🈯️📒🈯️💹🍀️  ",
+            "  ️🚹️🛃🎽💹💹🤢🈯️💚🍀           ️💹🤢💹🤮🈯️🌴  ",
+            "🛂🛅🚹️🈯️🈯️💹🐲💹🈯️🔫          ️💚🈯️💹🤢🦎💹🦎 ",
+            "🎽️️🚹️🛃🛂🎽💹💚📗🤢💹🐲          💹💹🐲🈯️💹🦎 ",
+            "🎽️🛂🚹️♿️🛃🌌🈯️💹💚🍀🈯️💚         ️🈯️🤢💚💹💚🌴",
+            "🎽️🛃🎽🛂🚹️🎽🍏🐲🈯️💹🤢💹🥦      💹🈯️🈯️🟢🈯️🦎 ",
+            "🎽️🚹️🛅🛃🔵🛂 💚🍀🐲💚🍀📗🦎    ️💚🐲💹🤢💹💹  ",
+            "🎽🛂♿️🈳🛅🛃   🔰💹📗🤢🈯️🍏🍀   🈯️📗🍀🈯️🐲🌴 ",
+            "🎽🚹️🎽🔵🚹️🎽     ️🍏💚💹💚🐲💹🥦 ️💹💚💹💚💹🦎 ",
+            "🎽🎽️🔵🛂🛃🔵     🍀📗🍀🟢💚🍀🌿🐲🍀🟢🐲🍀🍀  ",
+            "🎽🎽🚹️🔵🛂🚹️️       🥬💚🐲💹📗🟢💚📗💚💹🟢💚  ",
+            "🎽🔵💙🎽🔵🎽          🍏🍀💚🍀🐲💹🈯️🟢🍀📗🦎 ",
+            "🎽💙🎽🛂🎽🎽          🥬💚📗🥬🟢🈯️💹🍀💚🟢🌴 ",
+            "🎽🔵🚹️🔵💙🔵             🍀🟢💚🍀💹🟢📗💚🥬  ",
+            "🎽🎽💙🎽🚹️🔵              💚🥬🐲💚🈯️💹🍀🪀   ",
+            "  🎽🎽🔵🔵💙               🦠💚🟢💹🟢💚🥬    ",
+            "      ️🧿🔵💙                 🍀💚🈯️🍀🥬      ",
+            "        ️🎽🧿                  🐍�💚🟢💚      ",
+            "          🎽                     🥬🍀        ",
+            "                                             ",
+            "                                             ",
+          },
+          packages = {
+            enable = false,
           },
         },
       }
@@ -76,6 +125,130 @@ return {
       dependencies = "nvim-tree/nvim-web-devicons",
       config = function()
         local colors = require("base46").get_theme_tb "base_30"
+        -- Apply Base46 colors to bufferline highlights
+        vim.api.nvim_set_hl(0, "BufferLineBackground", {
+          fg = colors.light_grey,
+          bg = colors.black2,
+        })
+
+        vim.api.nvim_set_hl(0, "BufferlineIndicatorVisible", {
+          fg = colors.black2,
+          bg = colors.black2,
+        })
+
+        -- buffers
+        vim.api.nvim_set_hl(0, "BufferLineBufferSelected", {
+          fg = colors.white,
+          bg = colors.black,
+        })
+
+        vim.api.nvim_set_hl(0, "BufferLineBufferVisible", {
+          fg = colors.light_grey,
+          bg = colors.black2,
+        })
+
+        -- for diagnostics = "nvim_lsp"
+        vim.api.nvim_set_hl(0, "BufferLineError", {
+          fg = colors.light_grey,
+          bg = colors.black2,
+        })
+        vim.api.nvim_set_hl(0, "BufferLineErrorDiagnostic", {
+          fg = colors.light_grey,
+          bg = colors.black2,
+        })
+
+        -- close buttons
+        vim.api.nvim_set_hl(0, "BufferLineCloseButton", {
+          fg = colors.light_grey,
+          bg = colors.black2,
+        })
+        vim.api.nvim_set_hl(0, "BufferLineCloseButtonVisible", {
+          fg = colors.light_grey,
+          bg = colors.black2,
+        })
+        vim.api.nvim_set_hl(0, "BufferLineCloseButtonSelected", {
+          fg = colors.red,
+          bg = colors.black,
+        })
+        vim.api.nvim_set_hl(0, "BufferLineFill", {
+          fg = colors.grey_fg,
+          bg = colors.black2,
+        })
+        vim.api.nvim_set_hl(0, "BufferlineIndicatorSelected", {
+          fg = colors.black,
+          bg = colors.black,
+        })
+
+        -- modified
+        vim.api.nvim_set_hl(0, "BufferLineModified", {
+          fg = colors.red,
+          bg = colors.black2,
+        })
+        vim.api.nvim_set_hl(0, "BufferLineModifiedVisible", {
+          fg = colors.red,
+          bg = colors.black2,
+        })
+        vim.api.nvim_set_hl(0, "BufferLineModifiedSelected", {
+          fg = colors.green,
+          bg = colors.black,
+        })
+
+        -- separators
+        vim.api.nvim_set_hl(0, "BufferLineSeparator", {
+          fg = colors.black2,
+          bg = colors.black2,
+        })
+        vim.api.nvim_set_hl(0, "BufferLineSeparatorVisible", {
+          fg = colors.black2,
+          bg = colors.black2,
+        })
+        vim.api.nvim_set_hl(0, "BufferLineSeparatorSelected", {
+          fg = colors.black2,
+          bg = colors.black2,
+        })
+
+        -- tabs
+        vim.api.nvim_set_hl(0, "BufferLineTab", {
+          fg = colors.light_grey,
+          bg = colors.one_bg3,
+        })
+        vim.api.nvim_set_hl(0, "BufferLineTabSelected", {
+          fg = colors.black2,
+          bg = colors.nord_blue,
+        })
+        vim.api.nvim_set_hl(0, "BufferLineTabClose", {
+          fg = colors.red,
+          bg = colors.black,
+        })
+
+        vim.api.nvim_set_hl(0, "BufferLineDevIconDefaultSelected", {
+          bg = "none",
+        })
+
+        vim.api.nvim_set_hl(0, "BufferLineDevIconDefaultInactive", {
+          bg = "none",
+        })
+        vim.api.nvim_set_hl(0, "BufferLineDuplicate", {
+          fg = "NONE",
+          bg = colors.black2,
+        })
+        vim.api.nvim_set_hl(0, "BufferLineDuplicateSelected", {
+          fg = colors.red,
+          bg = colors.black,
+        })
+        vim.api.nvim_set_hl(0, "BufferLineDuplicateVisible", {
+          fg = colors.blue,
+          bg = colors.black2,
+        })
+
+        -- custom area
+        vim.api.nvim_set_hl(0, "BufferLineRightCustomAreaText1", {
+          fg = colors.white,
+        })
+
+        vim.api.nvim_set_hl(0, "BufferLineRightCustomAreaText2", {
+          fg = colors.red,
+        })
 
         require("bufferline").setup {
           options = {
@@ -90,39 +263,13 @@ return {
             show_close_icon = false,
             show_tab_indicators = true,
             always_show_bufferline = true,
-          },
-          highlights = {
-            fill = {
-              fg = colors.light_grey, -- Background of the bufferline
-              bg = colors.black2, -- Background of the bufferline
-            },
-            background = {
-              fg = colors.light_grey, -- Inactive buffer text
-              bg = colors.black2, -- Inactive buffer background
-            },
-            buffer_selected = {
-              fg = colors.white, -- Active buffer text
-              bg = colors.black, -- Active buffer background
-            },
-            buffer_visible = {
-              fg = colors.light_grey, -- Inactive buffer text
-              bg = colors.black2, -- Inactive buffer background
-            },
-            separator = {
-              fg = colors.black2, -- Separator color
-              bg = colors.black2,
-            },
-            separator_selected = {
-              fg = colors.black, -- Separator when buffer is active
-              bg = colors.black,
-            },
-            close_button = {
-              fg = colors.light_grey,
-              bg = colors.black2,
-            },
-            close_button_selected = {
-              fg = colors.red, -- Close button on active tab
-              bg = colors.black,
+            offsets = {
+              {
+                filetype = "NvimTree",
+                text = "File Explorer",
+                text_align = "left",
+                separator = true,
+              },
             },
           },
         }
